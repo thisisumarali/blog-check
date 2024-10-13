fe6d0c9f902efe97680007464ebf2b2e89e5813c
Umar khan <thisisumaralii@gmail.com>
2024-10-13 14:16:43 +0500

    Toast

--- app/page.js
+++ app/page.js
@@ -3,10 +3,13 @@ import Image from "next/image";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Bloglist from "./Components/Bloglist";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Home() {
   return (
     <div>
+      <ToastContainer theme="dark" />
       <Header />
       <Bloglist />
       <Footer />
